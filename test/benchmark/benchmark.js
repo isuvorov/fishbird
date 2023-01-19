40c62816a92dd3048c87d56ec77ecb44ff1e7434
Igor Suvorov <hi@isuvorov.com>
2023-01-19 12:39:06 +0300

    fix: clean benchmark

--- test/benchmark/benchmark.js
+++ test/benchmark/benchmark.js
@@ -13,12 +13,6 @@ function formatNumber(number) {
     .replace(/\d{3}$/, ',$&')
     .replace(/^(\d|\d\d)(\d{3},)/, '$1,$2');
 }
-
-// function formatNumber(number) {
-//   number = String(number).split('.');
-//   return number[0].replace(/(?=(?:\d{3})+$)(?!\b)/g, ',') +
-//     (number[1] ? '.' + number[1] : '');
-// }
 module.exports = mapSeries(
   suitesConfig
     .filter(({ skip }) => !skip)
@@ -42,36 +36,6 @@ module.exports = mapSeries(
           });
         suite
           .on('cycle', (event) => {
-            // function toStringBench() {
-            //   var bench = this,
-            //       error = bench.error,
-            //       hz = bench.hz,
-            //       id = bench.id,
-            //       stats = bench.stats,
-            //       size = stats.sample.length,
-            //       pm = '\xb1',
-            //       result = bench.name || (_.isNaN(id) ? id : '<Test #' + id + '>');
-
-            //   if (error) {
-            //     var errorStr;
-            //     if (!_.isObject(error)) {
-            //       errorStr = String(error);
-            //     } else if (!_.isError(Error)) {
-            //       errorStr = join(error);
-            //     } else {
-            //       // Error#name and Error#message properties are non-enumerable.
-            //       errorStr = join(_.assign({ 'name': error.name, 'message': error.message }, error));
-            //     }
-            //     result += ': ' + errorStr;
-            //   }
-            //   else {
-            //     result += ' x ' + formatNumber(hz.toFixed(hz < 100 ? 2 : 0)) + ' ops/sec ' + pm +
-            //       stats.rme.toFixed(2) + '% (' + size + ' run' + (size == 1 ? '' : 's') + ' sampled)';
-            //   }
-            //   return result;
-            // }
-            // console.log(`${String(event.target)}\n`);
-
             const { hz, name, stats, error } = event.target;
             if (error) {
               console.log(event.target);
