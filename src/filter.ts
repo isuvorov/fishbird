036b1ead2ce1ff02f02832d48f1e281b90c5e336
Igor Suvorov <hi@isuvorov.com>
2023-12-01 20:36:51 +0300

    feat: add filter functon

---
+++ src/filter.ts
@@ -0,0 +1,14 @@
+import { map } from './map.js';
+
+export async function filter<IN>(
+  arr: IN[],
+  filterFn: (item: Awaited<IN>) => boolean | Promise<boolean> = Boolean,
+): Promise<Awaited<IN>[]> {
+  const arr2 = await map(arr, async (i) => ({
+    i,
+    f: filterFn(i),
+  }));
+  return arr2.filter((i) => i.f).map((i) => i.i);
+}
+
+export default filter;
